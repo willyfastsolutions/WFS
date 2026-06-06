cc8e6f4994af0b93e8c694be85f953c1c0c91664
Willyfast Solutions Developer <developer@willyfastsolutions.com>
2026-06-05 21:12:26 -0500

    build: Set basePath to /WFS in next.config.ts when building on GitHub Actions

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,15 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS === "true";
+
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
+  // Set basePath only when deploying to GitHub Pages
+  basePath: isGithubActions ? '/WFS' : undefined,
 };
 
 export default nextConfig;
